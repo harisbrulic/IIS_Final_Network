6a0d3f42c517be8e1c24e31a4d7a032d3c1aae69
Haris Brulic <haris.brulic@student.um.si>
2026-06-06 23:03:05 +0200

    feat: testing

---
+++ src/tests/tests.py
@@ -0,0 +1,56 @@
+import sys
+import os
+import pandas as pd
+from evidently import Report
+from evidently.presets.dataset_stats import DataSummaryPreset
+from evidently.presets.drift import DataDriftPreset
+
+def test_threats_drift():
+    
+    os.makedirs("data/reference", exist_ok=True)
+    os.makedirs("reports", exist_ok=True)
+    
+    current = pd.read_csv("data/preprocessed/threats.csv")
+    reference_path = "data/reference/threats.csv"
+    
+    if not os.path.exists(reference_path):
+        print(f"Reference does not exist")
+        current.to_csv(reference_path, index=False)
+    
+    reference = pd.read_csv(reference_path)
+    
+    for df in [reference, current]:
+        if "first_seen" in df.columns:
+            del df["first_seen"]
+    
+    report = Report([
+        DataSummaryPreset(),
+        DataDriftPreset(),
+    ],
+    include_tests=True
+    )
+    
+    result = report.run(reference_data=reference, current_data=current)
+    
+    result.save_html("reports/data_testing_report.html")
+    
+    all_tests_passed = True
+    result_dict = result.dict()
+    if "tests" in result_dict:
+        for test in result_dict["tests"]:
+            if "status" in test and test["status"] != "SUCCESS":
+                all_tests_passed = False
+                break
+    
+    if not all_tests_passed:
+        print("Failed")
+        sys.exit(1)
+    else:
+        print("Done")
+        os.remove(reference_path)
+        current = pd.read_csv("data/preprocessed/threats.csv")
+        current.to_csv(reference_path, index=False)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    test_threats_drift()
